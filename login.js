1b5cec01772a7a7987363d4841babb1920dff52f
Ryan Wu <ryandev331@gmail.com>
2020-02-26 16:26:33 +0800

    add fb ad

---
+++ login.js
@@ -0,0 +1,21 @@
+// (function() {
+//     var testV = 1;
+//     var pass1 = prompt('Enter Your Password', ' ');
+//     while (testV < 3) {
+//         if (!pass1)
+//             history.go(-1);
+//         if (pass1.toLowerCase() == "gogofun42825384") {
+//             alert('Password Correct');
+//             // window.open("Yournextpage.html");
+//             break;
+//         }
+//         testV += 1;
+//         var pass1 =
+//             prompt('Access Denied - Password Incorrect, Please Try Again.', 'Password');
+//     }
+//     if (pass1.toLowerCase() != "password" && testV == 3) {
+//         history.go(-1);
+//     }
+
+//     return " ";
+// })()
